d9a35aa918bb632ffc25b07e5c607d6e5a5b2c3e
Charles Stockman <chuck.stockman@yahoo.com>
2015-10-20 01:35:30 -0400

    Add comments to the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A collection of functions to cache potentially time-consuming
+## computations and a function to demostrate how the caching functions
+## are used. The example function will assigned to the variable cacheSolve and
+## will return the inverse of a matrix
 
-## Write a short comment describing this function
-
+## A fucntion to cache the mean of a vector. There two steps
+## 1. if the mean has calculated then it stored.  Return the stored value
+## 2. if the mean has not been calculated then calculate the mean.  Retrurn the stored value 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
 
-
-## Write a short comment describing this function
-
+## An exmple function show how to the makeCacheMatrix function.
+## It will accept a matrix and return the inverse of a matrix. 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
